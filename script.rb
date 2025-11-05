c4761c373f201dc8f0411af3876c7a9d6a6d1900
Shane <97196288+Lanternray@users.noreply.github.com>
2025-11-05 14:18:50 -0600

    OOP Lesson/Practice

--- script.rb
+++ script.rb
@@ -1,52 +1,22 @@
-class MyCar
-    
-  attr_accessor :color
-  attr_reader :year
+class GoodDog
+    attr_accessor :name, :height, :weight
 
-  def initialize(year, model, color)
-      @year = year
-      @model = model
-      @color = color
-      @current_speed = 0
-  end
+    def initialize(n, h, w)
+        self.name = n
+        self.height = h
+        self.weight = w
+    end
 
-  def speed_up(number)
-      @current_speed += number
-      puts "You push the gas and accelerate #{number} mph."
-  end
+    def change_info(n, h, w)
+        self.name = n
+        self.height = h
+        self.weight = w
+    end
 
-  def brake(number)
-      @current_speed -= number
-      puts "You push the brake and decelerate #{number} mph."
-  end
-
-  def current_speed
-      puts "You are now going #{@current_speed} mph."
-  end
-
-  def shut_down
-      @current_speed = 0
-      puts "Let's park this bad boy!"
-  end
-
-  def spray_paint(color)
-      self.color = color
-      puts "Your new #{color} paint job looks great!"
-  end
-
+    def info
+        "#{self.name} weight #{self.weight} and is #{self.height} tall."
+    end
 end
 
-mustang = MyCar.new(2021, "Mustang Mach E", "Black")
-mustang.speed_up(20)
-mustang.current_speed
-mustang.speed_up(20)
-mustang.current_speed
-mustang.brake(20)
-mustang.current_speed
-mustang.brake(20)
-mustang.shut_down
-mustang.current_speed
-
-mustang.spray_paint("Dark Matter Gray Metallic")
-puts mustang.color
-puts mustang.year
+sparky = GoodDog.new("Sparky", "12 inches", "10 lbs")
+p sparky.what_is_self
