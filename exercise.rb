4b80447c58f29ff35fa98be1e348f460a733d4b0
Shane <97196288+Lanternray@users.noreply.github.com>
2025-11-11 10:01:13 -0600

    OOPS Inheritance lesson

--- exercise.rb
+++ exercise.rb
@@ -1,30 +1,26 @@
-class Viking
+class Cinema
+    attr_accessor :name, :location
 
-    attr_accessor :name, :age, :health, :strength
-
-    @@starting_health
-
-    def initialize(name, age, health, strength)
+    def initialize(name, location)
         @name = name
-        @age = age
-        @health = @@starting_health
-        @strength = strength
+        @location = location
+        @movies = []  
     end
 
-    def take_damage(damage)
-        self.health -= damage
-        self.shout("OUCH!")  
-    end
-
-    def shout(str)
-        puts str  
-    end
-
-    def sleep
-        health += 1 unless health >= 99  
+    def add_movie(movie)
+        @movies << movie
+        movie.cinema = self  
     end
 end
 
-oleg = Viking.new("Oleg", 19, 100, 8)
-p oleg.health
-p oleg.take_damage(10)
+class Movie
+    attr_accessor :title, :showtime, :cinema
+    
+    @@all = []
+
+    def initialize(title, showtime)
+        @title = title
+        @showtime = showtime
+        @@all << self  
+    end
+end
